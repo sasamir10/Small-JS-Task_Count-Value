b08216aa2138d8f8a7a1d2d1361e6989b586702a
Sabbir Ahmed Samir <141676624+sasamir10@users.noreply.github.com>
2025-11-26 22:08:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// alert("Be Careful, Dont waste time");
+
+let countValue = document.querySelector('#countValue');
+let decrease = document.querySelector('#decrease');
+let increase = document.querySelector('#increase');
+let reset = document.querySelector('#reset');
+
+
+
+const increaseFunction = () => {
+    let val = Number(countValue.innerHTML);
+    val += 1;
+
+    countValue.innerHTML = val;
+}
+
+const decreaseFunction = () => {
+    let val = Number(countValue.innerHTML);
+    val -= 1;
+
+    countValue.innerHTML = val;
+}
+
+const resetFunction = () => {
+    let val = Number(countValue.innerHTML);
+    // val = 0;
+
+    countValue.innerHTML = 0;
+}
+
+
+
+increase.addEventListener('click', increaseFunction);
+decrease.addEventListener('click', decreaseFunction);
+reset.addEventListener('click', resetFunction);
+
